4e8a11fb8d341fcbb1726da7fa4e16cc3e37a6e0
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-11 14:41:10 +0100

    Update: Create 'CategoryItem' and 'CategoryMenu' components on the app's homepage

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
+import CategoryMenu from "./components/category-menu/category-menu.component";
+
 const App = () => {
 
   const categories = [
-    {
-      id: 1,
-      title: 'Hats'
-    },
-    {
-      id: 2,
-      title: 'Jackets'
-    },
-    {
-      id: 3,
-      title: 'Sneakers'
-    },
-    {
-      id: 4,
-      title: 'Casual (Womens)'
-    },
-    {
-      id: 5,
-      title: 'Casual (Mens)'
-    },
-    {
-      id: 6,
-      title: 'African Traditional (Womens)'
-    },
-    {
-      id: 7,
-      title: 'African Traditional (Mens)'
-    },
-    {
-      id: 8,
-      title: 'Corporate (Womens)'
-    },
-    {
-      id: 9,
-      title: 'Corporate (Mens)'
-    }
-  ];
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens (casual)",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens (casual)",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  },
+  {
+    "id": 6,
+    "title": "womens (african)",
+    "imageUrl": "https://i.ibb.co/VHmjXJR/pexels-hamid-tajik-10919896.jpg"
+  },
+  {
+    "id": 7,
+    "title": "mens (african)",
+    "imageUrl": "https://i.ibb.co/v1Jfvyg/nigerian-man.jpg"
+  },
+  {
+    "id": 8,
+    "title": "bags",
+    "imageUrl": "https://i.ibb.co/5FYRhjL/bags.jpg"
+  }
+];
+
 
   return (
-    <div className="categories-container">
-      {
-        categories.map(({ id, title }) => (
-          <div className="category-container" key={ id }>
-            <div className="category-body-container">
-            {/* <img /> */}
-            <h2> { title } </h2>
-            <p> Shop Now </p>
-            </div>
-          </div>
-        ))
-      }  
-    </div>
+    <CategoryMenu categories={categories} />
   );
 }
 
