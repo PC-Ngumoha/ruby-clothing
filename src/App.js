ee3704075dc77f63bdf88964a2e5cc1d599d4b27
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-11 17:00:01 +0100

    Update: Minor changes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
-import CategoryMenu from "./components/category-menu/category-menu.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom';
+
+const Shop = () => {
+  return <h1>I'm the shop page</h1>
+};
 
 const App = () => {
-
-  const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens (casual)",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens (casual)",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  },
-  {
-    "id": 6,
-    "title": "womens (african)",
-    "imageUrl": "https://i.ibb.co/VHmjXJR/pexels-hamid-tajik-10919896.jpg"
-  },
-  {
-    "id": 7,
-    "title": "mens (african)",
-    "imageUrl": "https://i.ibb.co/v1Jfvyg/nigerian-man.jpg"
-  },
-  {
-    "id": 8,
-    "title": "bags",
-    "imageUrl": "https://i.ibb.co/5FYRhjL/bags.jpg"
-  }
-];
-
-
   return (
-    <CategoryMenu categories={categories} />
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
+      {/* <Route path="/home" element={<Home />} /> */}
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,58 @@
+import { Outlet } from "react-router-dom";
+import CategoryMenu from "../../components/category-menu/category-menu.component";
+
+const Home = () => {
+
+  const categories = [
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens (casual)",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens (casual)",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  },
+  {
+    "id": 6,
+    "title": "womens (african)",
+    "imageUrl": "https://i.ibb.co/VHmjXJR/pexels-hamid-tajik-10919896.jpg"
+  },
+  {
+    "id": 7,
+    "title": "mens (african)",
+    "imageUrl": "https://i.ibb.co/v1Jfvyg/nigerian-man.jpg"
+  },
+  {
+    "id": 8,
+    "title": "bags",
+    "imageUrl": "https://i.ibb.co/5FYRhjL/bags.jpg"
+  }
+];
+
+
+  return (
+    <div>
+      <Outlet />
+      <CategoryMenu categories={categories} />
+    </div>
+  );
+}
+
+export default Home;
