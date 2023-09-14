cd6eb58e07de92137f6cf7aa4666a579a781860c
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-14 11:01:35 +0100

    Update: Attempt to create responsive 'Navigation' component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 // Importing the SVG logo
@@ -8,15 +8,39 @@ import { ReactComponent as RubyLogo } from '../../assets/ruby-clothing.svg';
 import './navigation.style.scss';
 
 const Navigation = () => {
+  // Handles click state
+  // const [ windowWidth, setWindowWidth ] = useState(window.innerWidth);
+  // const [ clicked, setClicked ] = useState(false); 
+
+  // const clickChangeHandler = () => { setClicked(!clicked) };
+  // const updateWindowWidth = () => { setWindowWidth(window.innerWidth) };
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', updateWindowWidth);
+  //   if (windowWidth > 600) {
+  //     setClicked(true);
+  //   } else {
+  //     setClicked(false);
+  //   }
+  //   // return () => { window.removeEventListener('resize', updateWindowWidth) };
+  // }, [  windowWidth ]);
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/" >
           <RubyLogo className="logo" />
+
+          <a className="btn-toggle">
+            &#9776;
+          </a>
         </Link>
-        <div className="nav-links-container">
+        <div className= "nav-links-container" >
           <Link className="nav-link" to="/shop" >
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in" >
+            SIGN IN
           </Link>
         </div>
       </div>
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,10 @@
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1> I'm the SignIn Page </h1>
+    </div>
+  );
+};
+
+export default SignIn;
