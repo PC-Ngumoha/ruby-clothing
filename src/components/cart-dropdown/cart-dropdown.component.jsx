c1798f4141f47c426de8d6cffc9df5ff1de1b090
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-19 16:51:50 +0100

    Update: Create 'Checkout' component and functionality, also tried to make the currency expressions to be in 'Naira' as opposed to the 'Dollar' used by the instructor

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,16 @@
 import './cart-dropdown.style.scss';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckout = () => navigate('/checkout');
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -15,7 +20,7 @@ const CartDropdown = () => {
           )
         }
       </div>
-      <Button>Go To Checkout</Button>
+      <Button onClick={ goToCheckout }>Go To Checkout</Button>
     </div>
   );
 };
